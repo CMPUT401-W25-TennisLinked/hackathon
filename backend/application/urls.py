255695c3e0a539ad2603a22a0b30d7dd0b79e93c
Tammy Young <tammy.q.young@gmail.com>
2025-01-18 00:24:46 -0700

    add job application filter view and get by id view

--- backend/application/urls.py
+++ backend/application/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
-from .views import JobApplicationView
+from django.urls import path
+from .views import *
 
 
 namespace = "application"
 
 urlpatterns = [
-    path('', JobApplicationView.as_view(), name='job_application'),
+    path('', JobApplicationsView.as_view(), name='job_applications'),
+    path('<int:app_id>/', JobApplicationView.as_view(), name='job_application'),
+    path('search/', JobApplicationFilterView.as_view(), name='search_job_applications'),
 ]
--- backend/application/views.py
+++ backend/application/views.py
@@ -2,12 +2,29 @@ from django.views.generic import View
 from django.http import JsonResponse
 from .models import Application
 from .serializers import ApplicationSerializer
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
-class JobApplicationView(View):
+class JobApplicationsView(View):
 
     def get(self, request):
         applications = Application.objects.all()
         serializer = ApplicationSerializer(applications, many=True)
         return JsonResponse({'applications': serializer.data})
+
+
+class JobApplicationFilterView(View):
+
+    def get(self, request):
+        # TODO: get search params from request
+        applications = Application.objects.filter()
+        serializer = ApplicationSerializer(applications, many=True)
+        return JsonResponse({'applications': serializer.data})
+
+class JobApplicationView(View):
+
+    def get(self, request, app_id):
+        application = get_object_or_404(Application, pk=app_id)
+        serializer = ApplicationSerializer(application)
+        return JsonResponse({'application': serializer.data})
