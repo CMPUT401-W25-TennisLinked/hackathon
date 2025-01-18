a191a854a33f1ef5b59f28401b89bef3582b0987
Kele778 <100812664+Kele778@users.noreply.github.com>
2025-01-18 13:52:30 -0700

    Added to menu bar for filter (no logic)

---
+++ frontend/src/layout/Filter.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+import { Button} from '@headlessui/react'
+
+const options = [
+    {name:'Date'},
+    {name:'Status' , description:['Applied','Rejection','Interview', 'Offer']},
+  ]
+
+
+export default function Filter(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Function to toggle dialog
+    const handleToggleDialog = () => {
+      setIsOpen(!isOpen);
+    };
+  
+
+
+    return (
+        <Button onClick={handleToggleDialog} className="absolute flex flex-wrap pt-3 ml-auto" >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+    </svg>
+    {isOpen && (
+         <div className="w-children max-w-md flex-auto mt-10 overflow-hidden rounded-3xl bg-white text-sm/6 shadow-lg ring-1 ring-gray-900/5">
+         <div className="p-4">
+         {options.map((item) => (
+            
+         <div key={item.name} className="group relative flex gap-x-6 rounded-lg p-4 ">
+                <div>
+                  <a href={item.href} className="font-semibold text-gray-900">
+                    {item.name}
+                    <span className="absolute inset-0" />
+                  </a>
+                 
+                  
+                </div>
+                {item.description?.map((option) =>(
+                    <div className="hover:bg-gray-50">
+                    <Button key={option} className="mt-1 text-gray-600 ">{option}</Button>
+                    </div>
+                
+                ))}
+              </div>
+              
+         ))}
+               </div>
+             </div>
+      )}
+   
+    </Button>
+    )
+}
--- frontend/src/layout/SearchBar.js
+++ frontend/src/layout/SearchBar.js
@@ -1,15 +1,18 @@
 
+import Filter from "./Filter";
+
 
 const SearchBar = () => {
-
+    
+   
     // Code from https://www.creative-tim.com/twcomponents/component/searchbar
   return (
     <div class='max-w-md mx-auto flex gap-10 bg-red'>
     <div class="relative flex items-center w-full h-12 rounded-lg shadow-lg bg-grey overflow-hidden">
         <div class="grid place-items-center h-full w-12 text-gray-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
+            </svg> */}
         </div>
 
         <input
@@ -19,10 +22,7 @@ const SearchBar = () => {
         placeholder="Search something.." /> 
         
     </div>
-    <button >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-    </svg></button>
+   <Filter/>
 </div>
   );
 };
