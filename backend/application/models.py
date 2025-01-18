92c35b4ae4c2b15f3fa916c8f8e148da52050f86
Tammy Young <tammy.q.young@gmail.com>
2025-01-17 19:41:05 -0700

    add application model

--- backend/application/models.py
+++ backend/application/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('A', 'Accepted'),
+        ('R', 'Rejected'),
+    ]
+
+    company_name = models.CharField(max_length=256)
+    position = models.CharField(max_length=256)
+    date_applied = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=256, choices=STATUS_CHOICES, default='P')
+
+    def __str__(self):
+        return f"Application for {self.position} at {self.company_name}"
