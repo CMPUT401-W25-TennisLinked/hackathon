b85e50e431c57da07330d0394d51bd5da7b658e8
Tammy Young <tammy.q.young@gmail.com>
2025-01-18 12:28:02 -0700

    edit application status options

--- backend/application/models.py
+++ backend/application/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 class Application(models.Model):
     STATUS_CHOICES = [
-        ('P', 'Pending'),
-        ('A', 'Accepted'),
-        ('R', 'Rejected'),
+        ('APPLIED', 'Applied'),
+        ('INTERVIEW', 'Interview'),
+        ('OFFER', 'Offer'),
+        ('REJECTION', 'Rejection'),
     ]
 
     company_name = models.CharField(max_length=256)
     position = models.CharField(max_length=256)
     date_applied = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=256, choices=STATUS_CHOICES, default='P')
+    status = models.CharField(max_length=256, choices=STATUS_CHOICES, default='APPLIED')
 
     def __str__(self):
         return f"Application for {self.position} at {self.company_name}"
