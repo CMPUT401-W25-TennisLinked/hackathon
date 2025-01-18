12fc7b9b49c40596baee4d3bbaf847f31652cebd
Soban Shoaib <sobanshoaib7500@gmail.com>
2025-01-17 20:17:35 -0700

    worked on user app models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'application',
+    'job',
+    'notification',
+    'response',
+    'resume',
+    'user',
 ]
 
 MIDDLEWARE = [
--- backend/user/models.py
+++ backend/user/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Profile(models.Model):
     name = models.Charfield(max_length=50)
     email = models.EmailField(max_length=254)
-    phone = models.Charfield(max_length=50)
-    name = models.Charfield(max_length=50)
-    name = models.Charfield(max_length=50)
+    phone = models.Charfield(max_length=12)
+    city = models.Charfield(max_length=50)
+    country = models.Charfield(max_length=50)
+
+class Notifs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "notifications")
+    message = models.CharField(max_length=50)
+    read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now=False)
