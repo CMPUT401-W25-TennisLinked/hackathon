68540ab1413dab8cfd2c5aa6d8ba891f6512ecc1
Tammy Young <tammy.q.young@gmail.com>
2025-01-18 12:44:52 -0700

    update job menu page

--- frontend/src/layout/JobMenu.js
+++ frontend/src/layout/JobMenu.js
@@ -1,56 +1,61 @@
 import SearchBar from "./SearchBar"
+import { useState, useEffect } from 'react';
 
-const Jobs = [
-    { name: 'Job 1', status:'Applied'},
-    { name: 'Job 2', status: 'Rejected' },
-    { name: 'Job 3', status: 'Interview' },
-    { name: 'Job 4', status: 'Rejected' },
-    { name: 'Job 5', status: 'Interview' },
-    { name: 'Job 6', status: 'Offer' },
-  ]
 
-export default function JobMenu(){
+const statusColours = {
+    APPLIED: '#FFB347',
+    INTERVIEW: '#98FB98',
+    OFFER: '#87CEEB',
+    REJECTION: '#FFB6C1'
+}
+
+export default function JobMenu() {
     // Code adapted from https://tailwindflex.com/@lukas-muller/feature-showcase-cards 
     // SVG Icons from https://reactsvgicons.com/
-    return( <div>
-        <SearchBar />
-    
-    
-        
-    
-    <div class="-mx-4 flex flex-wrap pt-10 p-40">
-        {Jobs.map((item) =>(
-            <div class="w-full px-4 md:w-1/2 lg:w-3/2">
-            <div class="mb-9 rounded-xl py-8 px-7 shadow-md transition-all hover:shadow-lg sm:p-9 lg:px-6 xl:px-9">
-                <div style={{background:'rgb(199, 206, 212)',borderRadius:'15px',color: 'black',
-                    justifyContent:'center',alignContent:'center',display:'flex',fontFamily:'monospace',fontWeight:'bold',fontSize:'15px'}}>{item.status}</div>
-                <div class="mx-auto mt-5 mb-auto inline-block">     
-                    
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="4 0 20 30"
-                    width="5rem"
-                    height="5rem"
-                    >
-                    <path
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 7H5a2 2 0 0 0-2 2v4m5-6h8M8 7V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2m0 0h3a2 2 0 0 1 2 2v4m0 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6m18 0s-4 2-9 2s-9-2-9-2m9-2h.01"
-                    ></path></svg></div>
-                <div>
-                    <h3 class="mb-4 text-xl font-bold text-black sm:text-2xl lg:text-xl xl:text-2xl">{item.name}
-                    </h3>
-                    <p class="mx-5 text-base font-medium text-body-color">...</p>
-                </div>
-            </div>
-        </div>
-        ))}
-        
-    
 
-    </div>
+    const [applications, setApplications] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/application/')
+        .then((res) => res.json())
+        .then((data) => setApplications(data.applications))
+    }, []);
+
+    return (<div>
+        <SearchBar />
+
+        <div class="-mx-4 flex flex-wrap pt-10 p-40">
+            {applications.map((job) => (
+                <div class="w-full px-4 md:w-1/2 lg:w-3/2">
+                    <div class="mb-9 rounded-xl py-8 px-7 shadow-md transition-all hover:shadow-lg sm:p-9 lg:px-6 xl:px-9">
+                        <div style={{
+                            background: statusColours[job.status], borderRadius: '15px', color: 'black',
+                            justifyContent: 'center', alignContent: 'center', display: 'flex', fontFamily: 'monospace', fontWeight: 'bold', fontSize: '15px'
+                        }}>{job.status}</div>
+                        <div class="mx-auto mt-5 mb-auto inline-block">
+
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="4 0 20 30"
+                                width="5rem"
+                                height="5rem"
+                            >
+                                <path
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M8 7H5a2 2 0 0 0-2 2v4m5-6h8M8 7V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2m0 0h3a2 2 0 0 1 2 2v4m0 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6m18 0s-4 2-9 2s-9-2-9-2m9-2h.01"
+                                ></path></svg></div>
+                        <div>
+                            <h3 class="text-xl font-bold text-black sm:text-2xl lg:text-xl xl:text-2xl">{job.position}</h3>
+                            <p class="text-base text-body-color">{job.companyName}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+
+        </div>
     </div>)
 }
